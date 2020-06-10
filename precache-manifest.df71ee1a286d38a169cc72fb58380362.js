022ee2d4bee74825f00a8f02a96d35f220aa3ef4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-10 17:39:54 +0000

    Deploy BigBubbus/vuejs-twitch to github.com/BigBubbus/vuejs-twitch.git:gh-pages

---
+++ precache-manifest.df71ee1a286d38a169cc72fb58380362.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2517139db060528a032",
+    "url": "/vuejs-twitch/css/about.99415dee.css"
+  },
+  {
+    "revision": "35f06f8c8cb45b2f1572",
+    "url": "/vuejs-twitch/css/app.4b5652d6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuejs-twitch/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0aa84eac1387b135901ecba279a5b5bb",
+    "url": "/vuejs-twitch/index.html"
+  },
+  {
+    "revision": "e2517139db060528a032",
+    "url": "/vuejs-twitch/js/about.91893025.js"
+  },
+  {
+    "revision": "35f06f8c8cb45b2f1572",
+    "url": "/vuejs-twitch/js/app.5ea5a55c.js"
+  },
+  {
+    "revision": "d8dd547ac605f4f54d09",
+    "url": "/vuejs-twitch/js/chunk-vendors.907faaf9.js"
+  },
+  {
+    "revision": "e7ff2a519ce382d923057437eb486331",
+    "url": "/vuejs-twitch/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuejs-twitch/robots.txt"
+  }
+]);
