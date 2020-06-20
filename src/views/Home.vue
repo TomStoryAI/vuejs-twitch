a908480b6fb64070ac328fc2fb9dd8607519b014
Your Name <not@viadee.de>
2020-06-20 19:52:55 +0200

    Necessary text editing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Da best calculator" />
+    <HelloWorld msg="Welcome to Da very best calculator" />
   </div>
 </template>
 
